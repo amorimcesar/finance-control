ab2ed646ce7964db0645bc5f764c22e80e05bd59
César Amorim <84666621+amorimcesar@users.noreply.github.com>
2025-05-18 21:31:27 -0300

    add endpoint to register an user

--- src/backend/FinanceControl.Api/Controllers/UserController.cs
+++ src/backend/FinanceControl.Api/Controllers/UserController.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FinanceControl.Communication.Requests;
+using FinanceControl.Communication.Responses;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FinanceControl.Api.Controllers;
 
-[Route("api/[controller]")]
+[Route("[controller]")]
 [ApiController]
 public class UserController : ControllerBase
 {
     [HttpPost]
-    public IActionResult Register()
+    [ProducesResponseType(typeof(ResponseRegisteredUserJson),StatusCodes.Status201Created)]
+    public IActionResult Register(RequestRegisterJson request)
     {
         return Created();
     }
