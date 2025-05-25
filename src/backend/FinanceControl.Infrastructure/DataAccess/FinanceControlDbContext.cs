8663ba254b1a27ef752f6ab7e0377aa029f164e6
César Amorim <84666621+amorimcesar@users.noreply.github.com>
2025-05-25 20:57:42 -0300

    add FinanceControlDbContext for database configuration
    Includes initial setup for EF Core DbContext in the Infrastructure layer.

---
+++ src/backend/FinanceControl.Infrastructure/DataAccess/FinanceControlDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FinanceControl.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceControl.Infrastructure.DataAccess;
+
+public class FinanceControlDbContext : DbContext
+{
+    public FinanceControlDbContext(DbContextOptions options) : base(options) {}
+    public DbSet<User> Users { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(FinanceControlDbContext).Assembly);
+    }
+}
